f60e91f84e309d49c2a0d992975f11a558ebda1d
DELL_25 <DELL_25@DESKTOP-N7VTCUU>
2025-02-21 11:00:19 -0600

    "correcion de errores 1"

--- Proyecto_POO/src/tienda/Almacen.java
+++ Proyecto_POO/src/tienda/Almacen.java
@@ -1,5 +1,16 @@
 package tienda;
 
 public class Almacen {
+	
+		private int id;
+		private String name;
+		private double cantidad;
 
+		public void mostrar(){}
+		public Almacen(int id, String name, double cantidad) {
+		this.id = id;
+		this.name = name;
+		this.cantidad = cantidad;
+		}
+		
 }
--- Proyecto_POO/src/tienda/Tienda_Principal.java
+++ Proyecto_POO/src/tienda/Tienda_Principal.java
@@ -3,7 +3,11 @@ package tienda;
 public class Tienda_Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Almacen obj1 = new Almacen(1,"San",25);
+		
+		obj1.mostrar();
+
+		System.out.println("mensaje " );
 
 	}
 
